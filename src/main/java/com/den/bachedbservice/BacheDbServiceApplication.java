738b5edfc0735f7605ac10d6879397fcc39b0dae
Vignesh <svzvicky@gmail.com>
2024-03-06 08:10:02 +0530

    Spring Initializer

---
+++ src/main/java/com/den/bachedbservice/BacheDbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.den.bachedbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacheDbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacheDbServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/den/bachedbservice/BacheDbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.den.bachedbservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacheDbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
